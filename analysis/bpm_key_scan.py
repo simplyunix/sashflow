3329790abab1fbd3d18f11030aa9ca61b5232dfe
Sasi Chand <eyetnz@gmail.com>
2026-01-27 16:13:34 +1300

    Add Camelot key notation

--- analysis/bpm_key_scan.py
+++ analysis/bpm_key_scan.py
@@ -2,6 +2,16 @@ import sys
 import librosa
 import numpy as np
 
+CAMELOT_MAP = {
+    "C": "8B",  "G": "9B",  "D": "10B", "A": "11B", "E": "12B",
+    "B": "1B",  "F#": "2B", "C#": "3B", "G#": "4B",  "D#": "5B",
+    "A#": "6B", "F": "7B",
+
+    "Am": "8A",  "Em": "9A",  "Bm": "10A", "F#m": "11A", "C#m": "12A",
+    "G#m": "1A", "D#m": "2A", "A#m": "3A",  "Fm": "4A",  "Cm": "5A",
+    "Gm": "6A",  "Dm": "7A",
+}
+
 def estimate_bpm(y, sr):
     tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
     return round(float(tempo.item()), 1)
@@ -29,7 +39,9 @@ def analyze_track(path):
 
     print(f"Duration: {round(duration, 1)} sec")
     print(f"BPM: {bpm}")
-    print(f"Key: {key}")
+    # print(f"Key: {key}")
+    camelot = CAMELOT_MAP.get(key, "Unknown")
+    print(f"Key: {key}  |  Camelot: {camelot}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
